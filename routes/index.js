964ad5d36b7766d28cfa196fe5e343cd96b20cc9
Leonardo Facundes <leonardo.fm00@gmail.com>
2018-05-16 11:06:49 -0300

    reestruturei de forma completa os arquivos para o banco do webs-pev; ainda sem acesso ao mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express'),
     router = express.Router();
 
-router.get('/',function(req, res){
+router.get('/', function(req, res){
     res.status(201);
-    res.send('Hello Leonix')
+    res.send('Hello Leonix');
 });
 
-//stormtroppers
-router.use('/stormtropper', require('./stormtroppers'));
+//Users
+router.use('/users', require('./users'));
 
 module.exports = router;
