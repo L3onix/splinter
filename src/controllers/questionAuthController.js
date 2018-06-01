69af820c6812569a2516c9c3a506c06954be9de5
Leonardo Facundes <leonardo.fm00@gmail.com>
2018-06-01 17:28:49 -0300

    checar rota para adicionar questões

--- src/controllers/questionAuthController.js
+++ src/controllers/questionAuthController.js
@@ -1,11 +1,19 @@
 const express = require('express'),
     router = express.Router(),
-    authMiddleware = require('../middlewares/auth');
+    authMiddleware = require('../middlewares/auth'),
+    Question = require('../models/question');
 
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.send({ok: true, user: req.userId});
+router.post('/', async (req, res) => {
+    //res.send({ok: true, user: req.userId});
+    try{
+        const question = await Question.create(req.body);
+
+        res.status(200).send({question});
+    }catch(err){
+        return res.status(400).send({err: 'Erro ao criar nova questão'});
+    }
 });
 
-module.exports = router;
+module.exports = app => app.use('/questionsAuth', router);
