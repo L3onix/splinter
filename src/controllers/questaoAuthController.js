eb9cb1f90d8e61b8c46ee05d25815b7d12d1d6e9
Leonardo Facundes <leonardo.fm00@gmail.com>
2018-06-05 20:59:24 -0300

    atualizei rota (post)/questaoAuth/ para que somente professores possam criar questões

--- src/controllers/questaoAuthController.js
+++ src/controllers/questaoAuthController.js
@@ -1,21 +1,30 @@
 const express = require('express'),
     router = express.Router(),
     authMiddleware = require('../middlewares/auth'),
-    Question = require('../models/questao');
+    Question = require('../models/questao'),
+    Usuario = require('../models/user');
 
 router.use(authMiddleware);
 
-//TODO: Deve ser feita a verificação do usuário, se ele é professor
 //rota para criar questão
 router.post('/', async (req, res) => {
     //res.send({ok: true, user: req.userId});
-    try{
-        //console.log({body: req.});
-        //console.log({eixo: req.body.eixo});
-        const question = await Question.create({...req.body, user: req.userId});
+    const userId = req.userId;
 
-        res.status(200).send({question});
+    try{
+        //buscando usuário para verificar se ele é professor
+        const user = await Usuario.findById(userId);
+        //checando se é professor
+        if(user.professor === true){
+            const question = await Question.create({...req.body, user: req.userId});
+
+            res.status(200).send({question});
+        }else{
+            res.status(400).send({erro: 'Usuário não é professor'});
+        }
+
     }catch(err){
+        console.log(err);
         return res.status(400).send({error: 'Erro ao criar nova questão'});
     }
 });
