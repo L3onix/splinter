3f1b9f2aa23b70f277e7f5a8c1beaf07dedade02
Leonardo Facundes <leonardo.fm00@gmail.com>
2018-06-23 14:25:06 -0300

    criando rotas para sollução

--- src/controllers/solucaoAuthController.js
+++ src/controllers/solucaoAuthController.js
@@ -29,8 +29,36 @@ router.post('/:questaoId', async(req, res) => {
 });
 
 router.put('/:solucaoId', async (req, res) => {
-    
+    const userId = req.userId;
+    //console.log (userId);
+
+    try{
+        //bucando solução
+        const solucao = await Solucao.findById(req.params.solucaoId);
+
+        //se o id da requisição é igual o id do criador
+        if(solucao.createBy == req.userId){
+            const editado = await Solucao.findByIdAndUpdate(req.params.solucaoId, req.body, {new: true});
+            //res.status(200).send({upload: true});
+            res.status(200).send(editado);
+        }else{
+            res.status(400).send({erro: 'Usuário não é dono desta solução'});
+        }
+    }catch(err){
+        console.log(err);
+        res.status(400).send({erro: 'Erro ao tentar editar solução'});
+    }
 });
+
+router.get('/', async (req, res) => {
+    try{
+        solucoes = await Solucao.find();
+
+        return res.status(200).send({solucoes});
+    }catch(err){
+        return res.status(400).send({err: 'Erro ao buscar soluções'})
+    }
+})
 
 //exportar rota para rota principal
 module.exports = app => app.use('/solucaoAuth', router);
