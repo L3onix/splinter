ea193a99d19adb3cff7abbf1c270823ea09a36dc
Leonardo Facundes <leonardo.fm00@gmail.com>
2018-07-07 18:13:58 -0300

    trabalhando no controller Questao

--- src/controllers/questao/auth.js
+++ src/controllers/questao/auth.js
@@ -9,10 +9,8 @@ router.use(authMiddleware);
 //rota para criar Questao
 router.post('/', async (req, res) => {
     try{
-        //buscando usuário da requisição
-        //const user = await Usuario.findById(req.userId);
         //checando se o usuário é professor
-        if(checarProfessor(req.userId)){
+        if(await checarProfessor(req.userId)){
             //criando Questao
             await Questao.create({...req.body, criador: req.userId});
             res.status(200).send({status: 'Sucesso ao criar questão'});
@@ -28,14 +26,21 @@ router.post('/', async (req, res) => {
 //rota para editar Questao
 router.put('/:questaoId', async(req, res) => {
     try{
-        
+        //checando se o usuário é professor
+        if(await checarProfessor(req.userId)){
+            //editando Questao
+            await Questao.findByIdAndUpdate(req.params.questaoId, req.body);
+            res.status(200).send({status: 'Sucesso ao editar questão'});
+        }else{
+            res.status(400).send({status: 'Usuário não é professor'});
+        }
     }catch(err){
         console.log(err);
         res.status(400).send({status: 'Erro ao editar questão'});
     }
 });
 
-function checarProfessor(userId){
+async function checarProfessor(userId){
     const user = await Usuario.findById(userId);
     if(user.professor == true){
         return true;
