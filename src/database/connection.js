d3f301504f8cd1f0f402ecbf51861866b8eb4fe6
L3onix Leonardo Facundes <leonardo.fm00@gmail.com>
2018-10-25 14:56:06 -0300

    fazendo modificações para subir API na Umbler

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //conexão com banco de dados 'webs-pev'
-mongoose.connect('mongodb://localhost/webs-pev');
+mongoose.connect('mongodb://mestreninja:default1@naboo.mongodb.umbler.com:46941/webs-pev', {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
