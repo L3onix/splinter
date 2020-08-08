996dd20ac77224452ebf603a73b015369d952a82
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2020-08-08 19:20:17 -0300

    resolvendo warning de conexão do mongoose

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
+
 //MLAB
 mlabUser = 'developer';
 mlabPass = 'default1';
 mlabUrl = 'ds129045.mlab.com:29045/atena_db';
 mlab = 'mongodb://'+mlabUser+':'+mlabPass+'@'+mlabUrl;
 
-mongoose.connect(mlab, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(mlab, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
