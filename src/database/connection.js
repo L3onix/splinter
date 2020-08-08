c46fc3e0b096b0ba0b466e8f176aaafef38c6809
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2020-08-08 19:13:07 -0300

    ajustando conexão com mongoose

--- src/database/connection.js
+++ src/database/connection.js
@@ -6,7 +6,7 @@ mlabPass = 'default1';
 mlabUrl = 'ds129045.mlab.com:29045/atena_db';
 mlab = 'mongodb://'+mlabUser+':'+mlabPass+'@'+mlabUrl;
 
-mongoose.connect(mlab, {useNewUrlParser: true});
+mongoose.connect(mlab, {useUnifiedTopology: true, useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
