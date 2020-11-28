b45a3b63be6809d86d32e799b843daccd5f18a85
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2020-11-28 08:56:37 -0300

    adicionando rotas relacionadas a Comment

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ const express = require('express'),
     router = express.Router(),
     authMiddleware = require('./middlewares/auth'),
     AuthController = require('./controllers/authController'),
-    QuestionController = require('./controllers/question'),
-    SolutionController = require('./controllers/solution')
+    QuestionController = require('./controllers/questionController'),
+    SolutionController = require('./controllers/solutionController'),
+    CommentController = require('./controllers/commentController')
 
 
 router.post('/auth/register', (req, res) => {return new AuthController().register(req, res)})
@@ -19,5 +20,10 @@ router.post('/solution', authMiddleware, (req, res) => {return new SolutionContr
 router.put('/solution/:solutionId', authMiddleware, (req, res) => {return new SolutionController().update(req,res)})
 router.patch('/solution/:solutionId', authMiddleware, (req, res) => {return new SolutionController().toEvaluate(req,res)})
 router.delete('/solution/:solutionId', authMiddleware, (req, res) => {return new SolutionController().delete(req,res)})
+
+router.get('/comment/:commentId', (req, res) => {return new CommentController().list(req, res)})
+router.post('/comment', authMiddleware, (req, res) => {return new CommentController().create(req, res)})
+router.put('/comment/:commentId', authMiddleware, (req, res) => {return new CommentController().update(req, res)})
+router.delete('/comment/:commentId', authMiddleware, (req, res) => {return new CommentController().delete(req, res)})
 
 module.exports = app => app.use(router)
