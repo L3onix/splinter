91f779a903e0414e3abb5603330c8c505ee5719d
Leonardo Facundes l3onix <leonardo.fm00@gmail.com>
2023-11-01 22:35:33 -0300

    adjust routes to new auth methods

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,41 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const jwt = require('jsonwebtoken')
-const jwtValidate = require('../middlewares/jwt')
-
+const express = require("express");
+const router = express.Router();
+const jwtValidate = require("../middlewares/jwt");
+const { signInUser, signUpUser } = require("../controllers/AuthController");
 
 router.get("/status", jwtValidate, (req, res) => {
-    res.status(200).json(req.userInfo)
-})
+    res.status(200).json(req.userInfo);
+});
 
-router.post('/login', (req, res) => {
-    const { login, password } = req.body
-    if (login === 'teste' && password === 'default1') {
+router.post("/register", signUpUser);
+router.post("/login", signInUser);
 
-        const dadosUsuario = {
-            nome: "marcelo",
-            email: "teste@gmail.com",
-            id: 1
-        };
-        
-        const chavePrivada = "consolelog.com.br";
-
-        jwt.sign(dadosUsuario, chavePrivada, (err, token) => {
-            if (err) {
-                res
-                    .status(500)
-                    .json({ mensagem: "Erro ao gerar o JWT" });
-
-                return;
-            }
-
-            res.set("x-access-token", token);
-            res.end();
-        });
-    } else {
-        res.status(401)
-        res.end()
-    }
-})
-
-module.exports = router
+module.exports = router;
