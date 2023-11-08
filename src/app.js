96b073d46bcc8304f863eccf8feaae4e891f05d3
Leonardo Facundes l3onix <leonardo.fm00@gmail.com>
2023-11-08 00:06:11 -0300

    install swagger and cofigure initial doc version

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const auth = require('./routes/auth')
-const user = require('./routes/user')
-const dbConnection = require('./helpers/dbConnection')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+const auth = require("./routes/auth");
+const user = require("./routes/user");
+const dbConnection = require("./helpers/dbConnection");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-dbConnection()
+dbConnection();
 
-app.get('/', async (req, res) => {
-    res.send('hello world')
-})
+app.get("/", async (req, res) => {
+    res.send("hello world");
+});
 
-app.use('/auth', auth)
-app.use('/user', user)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-module.exports = app
+app.use("/auth", auth);
+app.use("/user", user);
+
+module.exports = app;
