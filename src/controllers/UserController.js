d3dd213e1f2fd8eb409eae1cf2209cf1fad2d18b
Leonardo Facundes l3onix <leonardo.fm00@gmail.com>
2023-11-08 00:09:54 -0300

    refactor removing extra code

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,18 +1,13 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
-async function create(req, res) {
-    try {
-        const userData = await req.body
-        await User.create( req.body ).then((created) => {
-            if (!created) return res.status(400)
-            res.status(201).json({ success: true, created})
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(404).json({success: false, error: error.message})
-    }
+async function create(name, email, password) {
+    return await User.create({
+        name: name,
+        email: email,
+        password: password,
+    });
 }
 
 module.exports = {
-    create
-}
+    create,
+};
