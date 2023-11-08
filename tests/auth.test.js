4cfb39baf8a39c9efae208693496ee9b0ea1ccdf
Leonardo Facundes l3onix <leonardo.fm00@gmail.com>
2023-11-08 00:11:19 -0300

    configure tests to mock an user

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,25 +1,43 @@
-const request = require('supertest')
-const app = require('../src/app')
+const request = require("supertest");
+const app = require("../src/app");
+const User = require("../src/models/User");
+const { encryptPassword } = require("../src/helpers/BcryptHelper");
+const { generateAccessToken } = require("../src/helpers/JwtHelper");
 
 describe("Test the AUTH paths", () => {
-  test("(GET)/auth/status: when user logged and correct request", (done) => {
-    request(app)
-      .get("/auth/status")
-      .set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21lIjoibWFyY2VsbyIsImVtYWlsIjoidGVzdGVAZ21haWwuY29tIiwiaWQiOjEsImlhdCI6MTY5Njg3NzQ4OH0.pj_P0n-iPO7f7Z2emCgUo7Yv4kd6DDxnu9WsXvXD0Qk')
-      .then((response) => {
-        expect(response.statusCode).toBe(200)
-        done()
-      })
-  })
+    let token = "";
+    const name = "Dejair Lee";
+    const email = "dejair@teste.com";
+    const password = "asdfasdf";
 
-  test("(POST)/auth/login: when correct user data and correct request", (done) => {
-    request(app)
-      .post("/auth/login")
-      .send({ login: "teste", password: "default1" })
-      .then((response) => {
-        expect(response.statusCode).toBe(200)
-        done()
-      })
-  })
-})
+    beforeEach(async () => {
+        await User.deleteMany({});
+        const user = await User.create({
+            name: name,
+            email: email,
+            password: String(encryptPassword(password)),
+        });
 
+        token = generateAccessToken(user);
+    });
+
+    test("(GET)/auth/status: when user logged and correct request", (done) => {
+        request(app)
+            .get("/auth/status")
+            .set("Authorization", token)
+            .then((response) => {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+    });
+
+    test("(POST)/auth/login: when correct user data and correct request", (done) => {
+        request(app)
+            .post("/auth/login")
+            .send({ email: email, password: password })
+            .then((response) => {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+    });
+});
